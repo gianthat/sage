082685087291822139b12a8bd3122a26c49c8a1a
reidelliott <reid@efeqdev.com>
2015-09-23 12:14:52 -0500

    Adds collapse nav

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,21 @@
-<header class="banner" role="banner">
+<header class="banner navbar" role="banner">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav role="navigation">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+    </div>
+    <div class="navbar-collapse collapse">
+      <nav role="navigation">
+        <?php
+        if (has_nav_menu('primary_navigation')) :
+          wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+        endif;
+        ?>
+      </nav>
+    </div> <!-- .navbar-collapse -->
   </div>
 </header>
